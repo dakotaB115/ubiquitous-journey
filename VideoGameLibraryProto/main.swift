ecd5006a5f9a8784f5c1aed56ea8f1d8d81b41bd
Dakota Brown <dakotabrown@macbook-pro-25.local>
2018-09-07 10:04:31 -0500

    Initial Commit

---
+++ VideoGameLibraryProto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibraryProto
+//
+//  Created by Dakota Brown on 9/7/18.
+//  Copyright © 2018 Dakota Brown. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
